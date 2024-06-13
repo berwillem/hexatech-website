b5d86700447a009ab89b32f1b0468b9297ddf678
Bouaouni Samy <bouaounisamy410@gmail.com>
2024-06-13 21:42:44 +0200

    .

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import "./Footer.css"
+
+function Footer() {
+    return (
+        <div>
+
+            
+            <div>
+
+                <h3>Contact us</h3>
+
+            </div>
+
+
+
+
+
+
+            <div></div>
+
+
+
+
+            <div></div>
+
+
+
+
+
+        </div>
+    )
+}
+
+export default Footer
